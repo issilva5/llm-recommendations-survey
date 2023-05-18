182db1f66bd7f3bd94988a5ac35bb855fd8734ef
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2023-05-18 14:34:07 -0300

    Add the PORT to .env file

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 
 import open_ai
 
+load_dotenv()
 app = Flask(__name__)
 CORS(app)
 
@@ -18,4 +21,4 @@ def evaluation():
     return ""
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=os.getenv("PORT"))
