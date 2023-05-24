643b45f2186dbd03d562794b9a1b74227127ca4f
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2023-05-24 09:25:45 -0300

    Generate session id

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
 import Survey from "./components/Survey";
 import { survey } from "./survey_model";
+import React, { useEffect } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
+  useEffect(() => {
+    let sessionId = localStorage.getItem('llm_rec_session_id');
+    if (!sessionId) {
+      sessionId = uuidv4();
+      localStorage.setItem('llm_rec_session_id', sessionId);
+    }
+  }, []);
 
   return <Survey  {...survey} />;
 
