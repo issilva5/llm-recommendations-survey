2d0822bbcc49dea5a118ad1d027e5dcf68047288
Ítallo Silva <itallo.silva@ccc.ufcg.edu.br>
2023-05-31 20:20:25 -0300

    Adding responsive support

--- frontend/src/components/survey_elements/questions/LikertQuestion/index.jsx
+++ frontend/src/components/survey_elements/questions/LikertQuestion/index.jsx
@@ -3,7 +3,7 @@ import styles from "./style.module.css";
 
 function LikertScaleQuestion(props) {
 
-    const [selectedOption, setSelectedOption] = useState(props.answer-1);
+    const [selectedOption, setSelectedOption] = useState(props.answer - 1);
 
     const handleOptionClick = (option) => {
         setSelectedOption(option);
@@ -14,15 +14,26 @@ function LikertScaleQuestion(props) {
         <div className={styles.likertScaleContainer}>
             <div className={styles.likertScaleOptions}>
                 {props.options.map((option, i) => (
-                    <button
-                        key={i}
-                        value={i}
-                        className={`${styles.likertScaleButton} ${selectedOption === i ? styles.selected : ''
-                            }`}
-                        onClick={() => handleOptionClick(i)}
-                    >
-                        {option}
-                    </button>
+                    <>
+                        <button
+                            key={i}
+                            value={i}
+                            className={`${styles.likertScaleButton} ${selectedOption === i ? styles.selected : ''
+                                }`}
+                            onClick={() => handleOptionClick(i)}
+                        >
+                            {option}
+                        </button>
+                        <label>
+                            <input
+                                type="radio"
+                                value={i}
+                                checked={selectedOption === i}
+                                onChange={() => handleOptionClick(i)}
+                            />
+                            {option}
+                        </label>
+                    </>
                 ))}
             </div>
         </div>
